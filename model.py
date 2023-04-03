a60886070c56f7c0e2f042f1e5cfe14b8018d4c0
Amir <amirvt92@gmail.com>
2023-04-03 16:48:29 -0400

    domain model

---
+++ model.py
@@ -0,0 +1,59 @@
+from datetime import date
+from typing import Optional, List
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
+
+
+class Batch:
+    def __init__(
+            self, reference: str, sku: str, qty: int, eta: Optional[date]) -> None:
+        self.eta = eta
+        self._purchased_quantity = qty
+        self.sku = sku
+        self.reference = reference
+        self._allocations = set()
+
+    def allocate(self, line):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line):
+        self._allocations.discard(line)
+
+    def can_allocate(self, line: OrderLine):
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+    @property
+    def available_quantity(self):
+        return self._purchased_quantity - self.allocated_quantity
+
+    @property
+    def allocated_quantity(self):
+        return sum(a.qty for a in self._allocations)
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+
+def allocate(line: OrderLine, batches: List[Batch]) -> Batch:
+    batch = next(batch for batch in sorted(batches) if batch.can_allocate(line))
+    batch.allocate(line)
+    return batch.reference
---
+++ test_model.py
@@ -0,0 +1,82 @@
+from datetime import datetime, timedelta
+
+import pytest
+
+from model import *
+
+today = datetime.today()
+tomorrow = datetime.today() + timedelta(days=1)
+next_week = datetime.today() + timedelta(days=7)
+
+
+def make_batch_and_line(sku, batch_qty, line_qty):
+    batch = Batch(reference="batch-001", sku=sku, qty=batch_qty, eta=date.today())
+    line = OrderLine(orderid='order-ref', sku=sku, qty=line_qty)
+    return batch, line
+
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch, line = make_batch_and_line("SMALL-TABLE", 20, 2)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
+
+
+def test_can_allocate_if_available_greater_than_required():
+    batch, line = make_batch_and_line("SMALL-TABLE", 20, 2)
+    assert batch.can_allocate(line)
+
+
+def test_cannot_allocate_if_available_smaller_than_required():
+    batch, line = make_batch_and_line("SMALL-TABLE", 1, 2)
+    assert batch.can_allocate(line) is False
+
+
+def test_can_allocate_if_available_equal_to_required():
+    batch, line = make_batch_and_line("SMALL-TABLE", 2, 2)
+    assert batch.can_allocate(line)
+
+
+def test_cannot_allocate_if_skus_do_not_match():
+    batch = Batch(reference="batch-001", sku="SMALL-TABLE", qty=20, eta=date.today())
+    line = OrderLine(orderid='order-ref', sku="BIG-TABLE", qty=2)
+    assert batch.can_allocate(line) is False
+
+
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("SMALL-TABLE", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
+
+
+def test_prefers_current_stock_batches_to_shipments():
+    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=today)
+    line = OrderLine("oref", "RETRO-CLOCK", 10)
+
+    allocate(line, [in_stock_batch, shipment_batch])
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
+
+
+def test_prefers_earlier_batches():
+    early = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=today)
+    late = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    later = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=next_week)
+    line = OrderLine("oref", "RETRO-CLOCK", 10)
+
+    allocate(line, [early, late, later])
+    assert early.available_quantity == 90
+    assert late.available_quantity == 100
+    assert later.available_quantity == 100
+
+    @pytest.mark.usefixtures()
+    def test_returns_allocated_batch_ref():
+        in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+        shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=today)
+        line = OrderLine("oref", "RETRO-CLOCK", 10)
+
+        batch = allocate(line, [in_stock_batch, shipment_batch])
+        assert batch.reference == "in-stock-batch"
