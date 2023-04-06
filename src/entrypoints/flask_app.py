08d987c8d2ec2242187aed5f8b74a372901ab3ff
Amir <amirvt92@gmail.com>
2023-04-06 14:21:07 -0400

    added e2e test

--- src/entrypoints/flask_app.py
+++ src/entrypoints/flask_app.py
@@ -1,13 +1,34 @@
-from flask import app, request, jsonify
+from datetime import datetime
+
+from flask import request, jsonify, Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+import config
 from src.adapters import orm, repository
 from src.domain import model
 from src.service_layer import services
 
 orm.start_mappers()
 get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
+app = Flask(__name__)
+
+
+@app.route("/batch/add", methods=['POST'])
+def batch_add_endpoint():
+    session = get_session()
+    repo = repository.SqlRepository(session)
+    eta = request.json['eta']
+    if eta is not None:
+        eta = datetime.fromisoformat(eta).date()
+    services.add_batch(
+        request.json['ref'],
+        request.json['sku'],
+        request.json['qty'],
+        eta,
+        repo, session
+    )
+    return 'OK', 201
 
 
 @app.route("/batch/allocate", methods=['POST'])
@@ -40,3 +61,5 @@ def batch_deallocate_endpoint():
     except (model.OutOfStock, services.InvalidSku) as e:
         return jsonify({'message': str(e)}), 400
     return jsonify({'batch_ref': batch_ref}), 202
+
+app.run()
--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,9 +1,33 @@
 import pytest
+import requests
 
-@pytest.mark.skip
+import config
+
+def post_to_add_batch(ref, sku, qty, eta):
+    url = config.get_api_url()
+    r = requests.post(
+        f'{url}/batch/add',
+        json={'ref': ref, 'sku': sku, 'qty': qty, 'eta': eta}
+    )
+    assert r.status_code == 201
+
+@pytest.mark.usefixtures('restart_api')
+@pytest.mark.usefixtures('postgres_db')
 def test_happy_path_returns_201_and_allocated_batch():
-    pass
+    post_to_add_batch("later", "sku", 100, '2020-01-02')
+    post_to_add_batch("early", "sku", 100, '2020-01-01')
+    post_to_add_batch("other", "other", 100, None)
+    url = config.get_api_url()
+    r = requests.post(
+        f'{url}/batch/allocate',
+        json={'orderid': 'orderid', 'sku': 'sku', 'qty': 3}
+    )
+    assert r.status_code == 201
+    assert r.json()['batch_ref'] == 'early'
+
+
 @pytest.mark.skip
 def test_unhappy_path_returns_400_and_error_message():
     pass
+
 
