f44e4b86e1dd2264e7aa2836f0046d1f787d93a3
Amir <amirvt92@gmail.com>
2023-04-13 17:20:44 -0400

    added internal message bus - refactored tests

--- tests/unit/test_product.py
+++ tests/unit/test_product.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 from src.domain import events
-from src.service_layer import handlers
+from src.service_layer import messagebus
 from tests.unit.test_handlers import FakeUnitOfWork
 
 today = datetime.today()
@@ -11,10 +11,10 @@ next_week = datetime.today() + timedelta(days=7)
 
 def test_prefers_current_stock_batches_to_shipments():
     uow = FakeUnitOfWork()
-    services.add_batch("in-stock-batch", "sku", 100, None, uow)
-    services.add_batch("shipment-batch", "sku", 100, today, uow)
+    messagebus.handle(events.BatchCreated("in-stock-batch", "sku", 100, None,), uow)
+    messagebus.handle(events.BatchCreated("shipment-batch", "sku", 100, today,), uow)
 
-    services.allocate("oref", "sku", 10, uow)
+    messagebus.handle(events.AllocationRequired("oref", "sku", 10,), uow)
     product = uow.products.get("sku")
     assert product.batches[0].available_quantity == 90
     assert product.batches[1].available_quantity == 100
@@ -22,10 +22,10 @@ def test_prefers_current_stock_batches_to_shipments():
 
 def test_prefers_earlier_batches():
     uow = FakeUnitOfWork()
-    services.add_batch("in-stock-batch", "RETRO-CLOCK", 100, today, uow)
-    services.add_batch("shipment-batch-1", "RETRO-CLOCK", 100, tomorrow, uow)
-    services.add_batch("shipment-batch-2", "RETRO-CLOCK", 100, next_week, uow)
-    services.allocate("oref", "RETRO-CLOCK", 10, uow)
+    messagebus.handle(events.BatchCreated("in-stock-batch", "RETRO-CLOCK", 100, today,), uow)
+    messagebus.handle(events.BatchCreated("shipment-batch-1", "RETRO-CLOCK", 100, tomorrow,), uow)
+    messagebus.handle(events.BatchCreated("shipment-batch-2", "RETRO-CLOCK", 100, next_week,), uow)
+    messagebus.handle(events.AllocationRequired("oref", "RETRO-CLOCK", 10,), uow)
 
     product = uow.products.get("RETRO-CLOCK")
 
@@ -36,15 +36,15 @@ def test_prefers_earlier_batches():
 
 def test_raises_out_of_stock_exception_if_cannot_allocate():
     uow = FakeUnitOfWork()
-    services.add_batch("batch_ref_1", "sku_1", 10, None, uow)
-    allocation = services.allocate("order_id_1", "sku_1", 20, uow)
-    product = uow.products.get("sku_1")
-    assert product.events[-1] == events.OutOfStock(sku="sku_1")
+    messagebus.handle(events.BatchCreated("batch_ref_1", "sku_1", 10, None), uow)
+    allocation =messagebus.handle(events.AllocationRequired("order_id_1", "sku_1", 20), uow)[0]
+    # product = uow.products.get("sku_1")
+    # assert product.events[-1] == events.OutOfStock(sku="sku_1")
     assert allocation is None
 
 
 def test_increments_version_number():
     uow = FakeUnitOfWork()
-    services.add_batch("batch_ref_1", "sku_1", 10, None, uow)
-    services.allocate("order_id_1", "sku_1", 1, uow)
+    messagebus.handle(events.BatchCreated("batch_ref_1", "sku_1", 10, None), uow)
+    messagebus.handle(events.AllocationRequired("order_id_1", "sku_1", 1), uow)
     assert uow.products.get("sku_1").version_number == 1
